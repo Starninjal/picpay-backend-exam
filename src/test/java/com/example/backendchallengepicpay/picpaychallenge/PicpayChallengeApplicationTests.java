7f444d99eed13e8ad817ad17bf11e7d60f92005f
Administrator <BR17johnn@gmail.com>
2023-12-05 01:43:16 -0300

    :feat initial configuration

---
+++ src/test/java/com/example/backendchallengepicpay/picpaychallenge/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendchallengepicpay.picpaychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
